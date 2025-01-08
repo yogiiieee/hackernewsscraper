de678f91f5c35beee9b6bf3a0615233d09bf59a2
Your Name <you@example.com>
2025-01-08 23:46:11 +0530

    added user-agents

--- hackernewsscraper/hackernewsscraper/settings.py
+++ hackernewsscraper/hackernewsscraper/settings.py
@@ -50,9 +50,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+DOWNLOADER_MIDDLEWARES = {
 #    "hackernewsscraper.middlewares.HackernewsscraperDownloaderMiddleware": 543,
-#}
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
